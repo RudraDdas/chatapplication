83d6d16c03ba3d9bf1c2909542f871a166ad0ac4
Rajatpani2 <panirajat2@gmail.com>
2022-03-12 05:16:32 +0530

    login page from handleing done in client

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -3,26 +3,51 @@ import React, { useState } from 'react'
 
 function Login() {
   const [ishidde, setIshide] = useState(false)
+  const [LoginData, setLoginData] = useState({
+    Email: "",
+    password:""
+  })
+
+  const [isErremail, setiSEmail] = useState("")
+  const [isErrpassword, setIspassword] = useState("")
+  
 
   const togglehideshow = (e) => {
     e.preventDefault()
    setIshide(!ishidde)
- }
+  }
+  const handleChange = (e) => {
+    setLoginData({ ...LoginData, [e.target.name]: e.target.value })
+    // setiSEmail("")
+    // setIspassword("")
+  }
+  const Loginhandlesubmit = (e) => {
+    if (LoginData.Email==="") {
+    setiSEmail("*required")
+    }
+    if (LoginData.password === "") {
+      setIspassword("*required")
+    } else {
+      setiSEmail("")
+      setIspassword("")
+    }
+    e.preventDefault()
+  }
   return (
     <>
       <form>
-      <span><label  className="labels" >Email</label>
+        <span className='passowrd_container' ><label className="labels" >Email</label><span className="unsubmitmsg">{isErremail}</span>
         <br/>
-        <input className='inputfields' placeholder="abc@gmail.com" type="text" name="mail_id" />
+        <input value={LoginData.Email} className='inputfields' placeholder="abc@gmail.com" type="text" name="Email" onChange={handleChange}/>
       </span>
       <br />
       <span className='passowrd_container'>
-        <label  className="labels" >Password</label>
+        <label  className="labels" >Password</label><span className="unsubmitmsg">{isErrpassword}</span>
         <br/>
-        <input className='inputfields'  id="password_field" placeholder="abc@123" type= {ishidde ? "text" : "password"} name="mail_id" />
+        <input value={LoginData.password} className='inputfields' onChange={handleChange} id="password_field" placeholder="abc@123" type= {ishidde ? "text" : "password"} name="password" />
         <button className='hideShow_btn'  onClick={togglehideshow} > { ishidde ? "Hide" : "Show"} </button>
       </span>
-      <button className='loginbtn' type='submit'>Login</button>
+      <button className='loginbtn' type='submit' onClick={Loginhandlesubmit}>Login</button>
       <button className='guestuserbtn'>Get Guest user credential</button>
       </form>
     
--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -17,18 +17,18 @@ function Signup() {
  }
   return (
     <form>
-      <span><label  className="labels" >Name</label>
+      <span className='passowrd_container'> <label  className="labels" >Name</label><span className="unsubmitmsg">*required</span>
         <br/>
         <input className='inputfields' placeholder="abac das" type="text" name="Name" />
       </span>
       <br />
-      <span><label  className="labels" >Email</label>
+      <span className='passowrd_container'><label  className="labels" >Email</label><span className="unsubmitmsg">*required</span>
         <br/>
         <input className='inputfields' placeholder="abc@gmail.com" type="text" name="mail_id" />
       </span>
       <br />
       <span className='passowrd_container'>
-        <label  className="labels" >Password</label>
+        <label  className="labels" >Password</label><span className="unsubmitmsg">*required</span>
         <br/>
         <input className='inputfields' type={ pswd_visibility ? "text":"password"} id="password_field" placeholder="abc@123" name="password" />
         <button className='hideShow_btn'  onClick={togglehideshow_1} > { pswd_visibility ? "Hide" : "Show"} </button>
@@ -36,7 +36,7 @@ function Signup() {
       </span>
       <br />
       <span className='passowrd_container'>
-        <label  className="labels" >Conform Password</label>
+        <label  className="labels" >Conform Password</label><span className="unsubmitmsg">*required</span>
         <br/>
         <input className='inputfields' type={ cPswd_visibility ? "text":"password"} id="password_field2" placeholder="abc@123" name="cpassword" />
         <button className='hideShow_btn'  onClick={togglehideshow_2} > { cPswd_visibility ? "Hide" : "Show"} </button>
@@ -44,6 +44,8 @@ function Signup() {
       </span>
       <br/>
       <span className='passowrd_container'>
+        <label className="labels" >Display picture</label><span className="unsubmitmsg">*required</span>
+        <br />
         <input  type="file" className='inputfields'  id="password_field" placeholder="abc@123" name="cpassword" />
       </span>
       <button className='loginbtn' id="signupbtn">Signup</button>
