abf1de52cc7d7b479c181bcdac22f35e4b74620b
Rajatpani2 <panirajat2@gmail.com>
2022-03-21 10:51:14 +0530

    demmo commit

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,48 @@ dotenv.config({path:"./config.env"})
 const app = express()
 const chats = require("./Data/data")
 const PORT = process.env.PORT || 8000
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const userRouter = require("./routes/userRouter")
+=======
+const userRoutes = require("./routes/userRouter")
+>>>>>>> Stashed changes
+=======
+const userRoutes = require("./routes/userRouter")
+>>>>>>> Stashed changes
+=======
+const userRoutes = require("./routes/userRouter")
+>>>>>>> Stashed changes
 
 
     // console.log(process.env.PORT)
 // console.log(process.env.DB)
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 app.use(express.json())// to accept the json data from the server
 
 app.use(userRouter)
 
+=======
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+app.use(express.json())
+
+
+app.use("/api/user" , userRoutes)
+// to accept the json data from the server
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 app.get("/", (req, res) => {
     res.send("api running")
 })
