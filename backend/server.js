06fad085fd27c98e37b2b7ee5bbfb5a0f931eb8b
Rajatpani2 <panirajat2@gmail.com>
2022-04-09 16:59:55 +0530

    internet connection chcker function added backend

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,20 @@ const app = express()
 const chats = require("./Data/data")
 const PORT = process.env.PORT || 8000
 const userRouter = require("./routes/userRouter")
-const {errorhandler , urlnotFound} = require("./middleware/errorHandle")
+const { errorhandler, urlnotFound } = require("./middleware/errorHandle")
+const dns = require("dns")
+const os = require("os")
 
 
+dns.lookup("www.google.com", (err, address, family) => {
+    if (err) {
+        console.log("not connected to internet")
+    }
+})
+// console.log(os.userInfo())
+
+// const daat = dns.getServers()
+// console.log(daat)
     // console.log(process.env.PORT)
 // console.log(process.env.DB)
 
